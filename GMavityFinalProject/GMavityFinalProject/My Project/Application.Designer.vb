099046397be6f3ce81a49e199172781ce835c507
Grace Mavity <mavige01@students.ipfw.edu>
2018-03-27 17:25:07 -0400

    All anticipated forms created, no content added yet.

--- GMavityFinalProject/GMavityFinalProject/My Project/Application.Designer.vb
+++ GMavityFinalProject/GMavityFinalProject/My Project/Application.Designer.vb
@@ -32,7 +32,7 @@ Namespace My
 
         <Global.System.Diagnostics.DebuggerStepThroughAttribute()> _
         Protected Overrides Sub OnCreateMainForm()
-            Me.MainForm = Global.GMavityFinalProject.Form1
+            Me.MainForm = Global.GMavityFinalProject.frmMain
         End Sub
     End Class
 End Namespace
---
+++ GMavityFinalProject/GMavityFinalProject/Sublist.vb
@@ -0,0 +1,3 @@
+﻿Public Class frmSublist
+
+End Class
